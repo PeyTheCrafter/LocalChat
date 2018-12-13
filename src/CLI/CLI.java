d0f938a36df4bff5396626c2ea1da4dac22886f1
pablo <peythecrafter@gmail.com>
2018-12-13 16:42:07 +0100

    New class for server and client CLI

---
+++ src/CLI/CLI.java
@@ -0,0 +1,45 @@
+package CLI;
+
+import java.net.InetAddress;
+import java.util.Scanner;
+
+import controller.client.Client;
+import controller.netScan.scanner.ServerScanner;
+import controller.server.Server;
+
+public class CLI {
+	public CLI() {
+
+	}
+
+	public void client() {
+		System.out.println("Scan servers? (y/n)");
+		String response = new Scanner(System.in).nextLine();
+		if (response.equals("y")) {
+			System.out.println("Port: ");
+			int scanPort = new Scanner(System.in).nextInt();
+			System.out.println("Threads: ");
+			int threads = new Scanner(System.in).nextInt();
+			try {
+				String address = InetAddress.getLocalHost().getHostAddress();
+				ServerScanner ss = new ServerScanner(address.substring(0, address.lastIndexOf(".")), scanPort, 50);
+				ss.scan(threads);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println("IP: ");
+		String ip = new Scanner(System.in).nextLine();
+		System.out.println("Port: ");
+		int port = new Scanner(System.in).nextInt();
+		System.out.println("Username: ");
+		String username = new Scanner(System.in).nextLine();
+		Client client = new Client(ip, port, username);
+		client.createClient();
+	}
+	
+	public void server() {
+		System.out.println("Port: ");
+		new Server(new Scanner(System.in).nextInt()).start();
+	}
+}
